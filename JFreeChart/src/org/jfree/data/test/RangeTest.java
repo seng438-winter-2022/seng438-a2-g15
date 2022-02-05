cac930e4b529799e5d572c6c5439de1c421cf7ee
Brooke Kindleman <brooke.kindleman@gmail.com>
2022-02-05 14:27:13 -0700

    feat: Added tests for method  -> Still some issues

--- JFreeChart/src/org/jfree/data/test/RangeTest.java
+++ JFreeChart/src/org/jfree/data/test/RangeTest.java
@@ -7,6 +7,10 @@ import org.junit.*;
 public class RangeTest {
     private Range refRange;
     
+    // NOTE FROM BROOKE: I am currently researching as to whether Range is an 
+    // inclusive range, only partially inclusive, or non-inclusive, as I am recieving
+    // conflicting results from both these tests, and my own research. 
+    
     @BeforeClass public static void setUpBeforeClass() throws Exception {
     }
 
@@ -29,6 +33,7 @@ public class RangeTest {
     	assertTrue("The range from -4.0 to 8.0 intersects the range from -8 to 16.", refRange.intersects(-4.0, 8.0));
     }
     
+    // Thinking about this. 
     @Test
     public void intersectsWithinBoundaryTouchingBoundary() {
     	assertTrue("The range from -8.0 to 16.0 intersects the range from -8.0 to 16.0", refRange.intersects(-8.0, 16.0));
@@ -44,16 +49,17 @@ public class RangeTest {
     	assertTrue("The range from -14.0 to 2.0 intersects the range from -8.0 to 16.0", refRange.intersects(-14.0, 2.0));
     }
     
+    // Thinking about this
     @Test
     public void intersectsOnlyTouchingLowerBoundary() {
-    	assertTrue("The range from -14 to -8 intersects the range from -8.0 to 16.0", refRange.intersects(-14.0, -8.0));
+    	assertFalse("The range from -14 to -8 intersects the range from -8.0 to 16.0", refRange.intersects(-14.0, -8.0));
     }
     
     @Test
     public void intersectsFullyBelowLowerBoundaryNotTouchingBoundary() {
     	assertFalse("The range from -43.0 to -16.0 does not intersect the range from -8.0 to 16.0", refRange.intersects(-43.0, -16.0));
     }
-    //////
+    
     @Test
     public void intersectsAboveUpperBoundaryAndWithinBoundary() {
     	assertTrue("The range from 3.0 to 28.0 intersects the range from -8.0 to 16.0", refRange.intersects(3.0, 28.0));
@@ -61,7 +67,7 @@ public class RangeTest {
     
     @Test
     public void intersectsOnlyTouchingUpperBoundary() {
-    	assertTrue("The range from 16.0 to 32.0 intersects the range from -8.0 to 16.0", refRange.intersects(16.0, 32.0));
+    	assertFalse("The range from 16.0 to 32.0 intersects the range from -8.0 to 16.0", refRange.intersects(16.0, 32.0));
     }
     
     @Test
@@ -69,6 +75,31 @@ public class RangeTest {
     	assertFalse("The range from 17.0 to 19.0 does not intersect the range from -8.0 to 16.0", refRange.intersects(17.0, 19.0));
     }
     
+    @Test
+    public void containsBelowLowerBoundary() {
+    	assertFalse("The value -22. is not contained within the range from -8.0 to 16.0", refRange.contains(-22.0));
+    }
+    
+    @Test
+    public void containsOnLowerBoundary() {
+    	assertTrue("The value -8.0 is contained within the range from -8.0 to 16.0", refRange.contains(-8.0));
+    }
+
+    @Test
+    public void containsBetweenUpperAndLowerBoundary(){
+	    assertTrue("The value 0.0 is contained within the range from -8.0 to 16.0", refRange.contains(0.0));
+    }
+
+    @Test
+    public void containsOnUpperBoundary(){
+	    assertTrue("The value 16.0 is contained within the range from -8.0 to 16.0", refRange.contains(16.0));
+    }
+
+    @Test 
+    public void containsAboveUpperBoundary(){
+    	    assertFalse("The value 99.0 is not contained within the range from -8.0 to 16.0", refRange.contains(99.0));
+    }
+  
     
     @After
     public void tearDown() throws Exception {
