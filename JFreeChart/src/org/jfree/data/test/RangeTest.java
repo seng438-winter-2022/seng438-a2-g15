0ac561618579f9256b7d47ada79e65e5fc6ee40b
Robert Brown <brown.rjb75@gmail.com>
2022-02-04 18:28:12 -0700

    feat: implement unit tests for Range class implemented tests for the following methods: - Range.expandToInclude() - Range.shift() - Range.scale()

--- JFreeChart/src/org/jfree/data/test/RangeTest.java
+++ JFreeChart/src/org/jfree/data/test/RangeTest.java
@@ -6,21 +6,123 @@ import org.junit.*;
 
 public class RangeTest {
     private Range exampleRange;
+    private Range higherRange;
     @BeforeClass public static void setUpBeforeClass() throws Exception {
     }
 
 
     @Before
-    public void setUp() throws Exception { exampleRange = new Range(-1, 1);
+    public void setUp() throws Exception { 
+    	exampleRange = new Range(-1, 1);
+        higherRange = new Range(2,4);
     }
-
 
     @Test
-    public void centralValueShouldBeZero() {
-        assertEquals("The central value of -1 and 1 should be 0",
-        0, exampleRange.getCentralValue(), .000000001d);
+    public void expandToIncludeValidRangeAboveRange() {
+        Range testRange = Range.expandToInclude(exampleRange, 2);
+        assertEquals("The upper bound should be 2", 2, testRange.getUpperBound(), .000000001d);
+        assertEquals("The lower bound should be -1", -1, testRange.getLowerBound(), .000000001d);
     }
 
+    @Test
+    public void expandToIncludeValidRangeBelowRange() {
+        Range testRange = Range.expandToInclude(exampleRange, -2);
+        assertEquals("The upper bound should be 1", 1, testRange.getUpperBound(), .000000001d);
+        assertEquals("The lower bound should be -2", -2, testRange.getLowerBound(), .000000001d);
+    }
+
+    @Test
+    public void expandToIncludeValidRangeInRange() {
+        Range testRange = Range.expandToInclude(exampleRange, 0);
+        assertEquals("The upper bound should be 1", 1, testRange.getUpperBound(), .000000001d);
+        assertEquals("The lower bound should be -1", -1, testRange.getLowerBound(), .000000001d);
+    }
+
+    @Test
+    public void expandToIncludeValidRangeUpperBound() {
+        Range testRange = Range.expandToInclude(exampleRange, 1);
+        assertEquals("The upper bound should be 1", 1, testRange.getUpperBound(), .000000001d);
+        assertEquals("The lower bound should be -1", -1, testRange.getLowerBound(), .000000001d);
+    }
+
+    @Test
+    public void expandToIncludeValidRangeLowerBound() {
+        Range testRange = Range.expandToInclude(exampleRange, -1);
+        assertEquals("The upper bound should be 1", 1, testRange.getUpperBound(), .000000001d);
+        assertEquals("The lower bound should be -1", -1, testRange.getLowerBound(), .000000001d);
+    }
+    
+    @Test
+    public void expandToIncludeNullRangeAboveRange() {
+        Range testRange = Range.expandToInclude(null, 2);
+        assertEquals("The upper bound should be 2", 2, testRange.getUpperBound(), .000000001d);
+        assertEquals("The lower bound should be 2", 2, testRange.getLowerBound(), .000000001d);
+    }
+
+    @Test
+    public void shiftValidRangeGreaterThanZero() {
+        Range testRange = Range.shift(higherRange, 2);
+        assertEquals("The upper bound should be 6", 6, testRange.getUpperBound(), .000000001d);
+        assertEquals("The lower bound should be 4", 4, testRange.getLowerBound(), .000000001d);
+    }
+
+    @Test
+    public void shiftValidRangeEqualsZero() {
+        Range testRange = Range.shift(exampleRange, 0);
+        assertEquals("The upper bound should be 1", 1, testRange.getUpperBound(), .000000001d);
+        assertEquals("The lower bound should be -1", -1, testRange.getLowerBound(), .000000001d);
+    }
+
+    @Test
+    public void shiftValidRangeLessThanZero() {
+        Range testRange = Range.shift(higherRange, -1);
+        assertEquals("The upper bound should be 3", 3, testRange.getUpperBound(), .000000001d);
+        assertEquals("The lower bound should be 1", 1, testRange.getLowerBound(), .000000001d);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shiftNullRangeLessThanZero() {
+        Range testRange = Range.shift(null, -2);
+    }
+
+    @Test
+    public void shiftValidRangeCrossesZero() {
+        Range testRange = Range.shift(exampleRange, 2);
+        assertEquals("The upper bound should be 3", 3, testRange.getUpperBound(), .000000001d);
+        assertEquals("The lower bound should be 0", 0, testRange.getLowerBound(), .000000001d);
+    }
+
+    @Test
+    public void scaleValidRangeGreaterThanOne() {
+        Range testRange = Range.scale(exampleRange, 2);
+        assertEquals("The upper bound should be 2", 2, testRange.getUpperBound(), .000000001d);
+        assertEquals("The lower bound should be -2", -2, testRange.getLowerBound(), .000000001d);
+    }
+
+    @Test
+    public void scaleValidRangeEqualsOne() {
+        Range testRange = Range.scale(exampleRange, 1);
+        assertEquals("The upper bound should be 1", 1, testRange.getUpperBound(), .000000001d);
+        assertEquals("The lower bound should be -1", -1, testRange.getLowerBound(), .000000001d);
+    }
+
+    @Test
+    public void scaleValidRangeLessThanOne() {
+        Range testRange = Range.scale(exampleRange, 0.5);
+        assertEquals("The upper bound should be 0.5", 0.5, testRange.getUpperBound(), .000000001d);
+        assertEquals("The lower bound should be -0.5", -0.5, testRange.getLowerBound(), .000000001d);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void scaleValidRangeLessThanZero() {
+        Range testRange = Range.scale(exampleRange, -1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void scaleNullRangeLessThanOne() {
+        Range testRange = Range.scale(null, 2);
+    }
+    
     @After
     public void tearDown() throws Exception {
     }
