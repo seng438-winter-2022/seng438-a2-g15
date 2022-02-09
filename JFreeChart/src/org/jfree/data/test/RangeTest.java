eb8e6394595736c7cc898d57e668c11775366d96
Brooke Kindleman <brooke.kindleman@gmail.com>
2022-02-09 09:24:41 -0700

    feat: Added extreme edge cases for contains and intersects methods

--- JFreeChart/src/org/jfree/data/test/RangeTest.java
+++ JFreeChart/src/org/jfree/data/test/RangeTest.java
@@ -7,6 +7,8 @@ import org.junit.*;
 public class RangeTest {
     private Range refRange;
     private Range testRange;
+    private Range smallestRange;
+    
     
     @BeforeClass public static void setUpBeforeClass() throws Exception {
     }
@@ -15,6 +17,7 @@ public class RangeTest {
     @Before
     public void setUp() throws Exception { 
     	refRange = new Range (-8.0, 16.0);
+    	smallestRange = new Range(0.0, Double.MIN_VALUE);
   
     }
 
@@ -65,6 +68,16 @@ public class RangeTest {
     public void intersectsFullyAboveUpperBoundaryNotTouchingBoundaryDoubles() {
     	assertFalse("The range from 17.0 to 19.0 does not intersect the range from -8.0 to 16.0", refRange.intersects(17.0, 19.0));
     }
+    
+    @Test public void interesectsMinDoubleValToMaxDoubleValDoubles() {
+    	assertTrue("The range from MIN_VALUE for double to MAX_VALUE for double intersects with the range from -8.0 to 16.0", refRange.intersects(Double.MIN_VALUE, Double.MAX_VALUE));
+    }
+    
+    @Test public void intersectSmallestPossibleRangeDoubles() {
+    	assertTrue("The range from 0.0 to MIN_VALUE for double intersects with the range from -8.0 to 16.0", refRange.intersects(0.0, Double.MIN_VALUE));
+    }
+    
+    
     
     /////
     
@@ -126,6 +139,16 @@ public class RangeTest {
     	assertFalse("The range from 200.75 to 302.0 does not intersect the range from -8.0 to 16.0", refRange.intersects(testRange));
     }
     
+    @Test public void interesectsMinDoubleValToMaxDoubleValRangeArg() {
+    	testRange = new Range(Double.MIN_VALUE, Double.MAX_VALUE);
+    	assertTrue("The range from MIN_VALUE for double to MAX_VALUE for double intersects with the range from -8.0 to 16.0", refRange.intersects(testRange));
+    }
+    
+    @Test public void intersectSmallestPossibleRangeArgs() {
+    	assertTrue("The range from 0.0 to MIN_VALUE for double intersects with the range from -8.0 to 16.0", refRange.intersects(smallestRange));
+    }
+    
+    
     
     @Test
     public void containsBelowLowerBoundary() {
@@ -150,6 +173,16 @@ public class RangeTest {
     @Test 
     public void containsAboveUpperBoundary(){
     	    assertFalse("The value 99.0 is not contained within the range from -8.0 to 16.0", refRange.contains(99.0));
+    }
+    
+    @Test
+    public void containsMaxDoubleValue() {
+    	assertFalse("The maximum value of a double is not contained within the range from -8.0 to 16.0", refRange.contains(Double.MAX_VALUE));
+    }
+    
+    @Test
+    public void containsMinDoubleValue() {
+    	assertTrue("The minimum value of a double is contained within the range from -8.0 to 16.0", refRange.contains(Double.MIN_VALUE));
     }
   
     
